8a41f23955f33bd133c74b3cd45d4f529c7fd48b
Matthieu Nadini <matthieu.nadini@gmail.com>
2021-02-16 18:18:47 +0100

    Add

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setuptools.setup(
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-        "Operating System :: Linux",
+        "Operating System :: POSIX :: Linux",
     ],
     python_requires='>=3.6',
 )
