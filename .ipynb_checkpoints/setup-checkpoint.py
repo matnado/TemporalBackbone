5ab896970262bc6e90f400518a2ea4e89425b048
Matthieu Nadini <matthieu.nadini@gmail.com>
2021-02-16 19:32:26 +0100

    Add Sample code

--- .ipynb_checkpoints/setup-checkpoint.py
+++ .ipynb_checkpoints/setup-checkpoint.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="TemporalBackbone",
-    version="0.0.2",
+    version="1.0.0",
     author="Matthieu Nadini",
     author_email="matthieu.nadini@gmail.com",
     description="A tool to detect the backbone in temporal networks",
